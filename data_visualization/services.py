22fe68a99112c827b08bc9dda4533a4644b4d834
akicestmoi <tngch.aki@gmail.com>
2025-10-22 13:05:59 +0200

    further cleaned data visualization views.py and services.py

--- data_visualization/services.py
+++ data_visualization/services.py
@@ -1,9 +1,17 @@
+from datetime import date, timedelta
 from enum import Enum
 from typing import List, Optional, TypedDict
 
 import pandas as pd
+from dateutil.relativedelta import relativedelta
 
-from market_overview.models import AssetClassChoices, LocationChoices, MarketPriceModel
+import market_overview.services as market_overview_services
+from market_overview.models import (
+    AssetClassChoices,
+    AssetTypeChoices,
+    LocationChoices,
+    MarketPriceModel,
+)
 from market_overview.services import ASSETS_ORDER, AssetNames, calculate_price_change
 
 
@@ -96,7 +104,7 @@ def _sort_by_location_and_maturity(
     ).sort_values(["location_order", "maturity"], na_position="last")
 
 
-def format_data_for_display(
+def format_data_for_market_recap_display(
     reference_market_prices: List[MarketPriceModel],
     previous_market_prices: List[MarketPriceModel],
 ) -> List[DisplayData]:
@@ -141,3 +149,259 @@ def format_data_for_display(
             )
 
     return data_to_display
+
+
+class ChartDuration(str, Enum):
+    """Chart duration."""
+
+    ONE_WEEK = "1W"
+    ONE_MONTH = "1M"
+    YEAR_TO_DATE = "YTD"
+    ONE_YEAR = "1Y"
+
+
+def _get_start_date(reference_date: date, chart_duration: ChartDuration) -> date:
+    """Get start date for data."""
+    if chart_duration == ChartDuration.ONE_WEEK:
+        return reference_date - timedelta(weeks=1)
+    elif chart_duration == ChartDuration.ONE_MONTH:
+        return reference_date - relativedelta(months=1)
+    elif chart_duration == ChartDuration.YEAR_TO_DATE:
+        return reference_date - relativedelta(days=365)
+    elif chart_duration == ChartDuration.ONE_YEAR:
+        return reference_date - relativedelta(years=1)
+    else:
+        return reference_date
+
+
+class MarketChartsDropdownValues(TypedDict):
+    """Dropdown values."""
+
+    stocks: list[str]
+    fx: list[str]
+    crypto: list[str]
+    commodity: list[str]
+    rates: list[str]
+    locations: list[str]
+
+
+def get_market_charts_dropdown_values() -> MarketChartsDropdownValues:
+    """Get dropdown values."""
+    asset_classes = {
+        "stocks": {"asset_class": AssetClassChoices.STOCKS},
+        "fx": {"asset_class": AssetClassChoices.FX},
+        "crypto": {"asset_class": AssetClassChoices.CRYPTO},
+        "commodity": {"asset_class": AssetClassChoices.COMMODITIES},
+        "rates": {"asset_type": AssetTypeChoices.GOVERNMENT_BOND_RATE},
+    }
+    dropdown_values = {
+        name: market_overview_services.get_asset_names(filters)
+        for name, filters in asset_classes.items()
+    }
+    dropdown_values["locations"] = [str(choice.label) for choice in LocationChoices]
+    return dropdown_values
+
+
+class MarketChartsFrontData(TypedDict):
+    """Front data."""
+
+    reference_date: date
+    previous_curve_date: date
+    yield_curve_location: str
+    stock_name: str
+    fx_name: str
+    crypto_name: str
+    commodity_name: str
+    main_rate: str
+    spread_rate: str
+    stock_chart_duration: ChartDuration
+    fx_chart_duration: ChartDuration
+    crypto_chart_duration: ChartDuration
+    commodity_chart_duration: ChartDuration
+    spread_rates_chart_duration: ChartDuration
+    stock_name_compare: str
+    fx_name_compare: str
+    crypto_name_compare: str
+    commodity_name_compare: str
+
+
+class MarketChartsLabels(TypedDict):
+    """Labels."""
+
+    stocks: str
+    fx: str
+    crypto: str
+    commodity: str
+    yield_curve_location: str
+    main_rate: str
+    spread_rate: str
+    stock_compare: Optional[str]
+    fx_compare: Optional[str]
+    crypto_compare: Optional[str]
+    commodity_compare: Optional[str]
+
+
+def get_market_charts_labels(
+    dropdown_values: MarketChartsDropdownValues, front_data: MarketChartsFrontData
+) -> MarketChartsLabels:
+    """Get labels."""
+
+    def _generic_asset_label_getter(
+        asset_class: str, asset_name: Optional[str]
+    ) -> Optional[str]:
+        return (
+            get_asset_full_name(dropdown_values[asset_class], asset_name)
+            if asset_name
+            else None
+        )
+
+    return {
+        "stocks": _generic_asset_label_getter("stocks", front_data["stock_name"]),
+        "fx": _generic_asset_label_getter("fx", front_data["fx_name"]),
+        "crypto": _generic_asset_label_getter("crypto", front_data["crypto_name"]),
+        "commodity": _generic_asset_label_getter(
+            "commodity", front_data["commodity_name"]
+        ),
+        "yield_curve_location": front_data["yield_curve_location"],
+        "main_rate": front_data["main_rate"],
+        "spread_rate": front_data["spread_rate"],
+        "stock_compare": _generic_asset_label_getter(
+            "stocks", front_data["stock_name_compare"]
+        ),
+        "fx_compare": _generic_asset_label_getter("fx", front_data["fx_name_compare"]),
+        "crypto_compare": _generic_asset_label_getter(
+            "crypto", front_data["crypto_name_compare"]
+        ),
+        "commodity_compare": _generic_asset_label_getter(
+            "commodity", front_data["commodity_name_compare"]
+        ),
+    }
+
+
+class MarketChartsMarketData(TypedDict):
+    """Market data."""
+
+    stock_prices: list[dict]
+    stock_prices_compare: list[dict]
+    fx_prices: list[dict]
+    fx_prices_compare: list[dict]
+    crypto_prices: list[dict]
+    crypto_prices_compare: list[dict]
+    commodity_prices: list[dict]
+    commodity_prices_compare: list[dict]
+    reference_yield_curve: list[dict]
+    previous_yield_curve: list[dict]
+    spread_rates: list[dict]
+
+
+def get_market_charts_market_data(
+    reference_date: date, previous_curve_date: date, front_data: MarketChartsFrontData
+) -> MarketChartsMarketData:
+    """Get market data."""
+    main_rate_historical_yield = market_overview_services.get_historical_prices(
+        front_data["main_rate"],
+        start_date=_get_start_date(
+            reference_date, front_data["spread_rates_chart_duration"]
+        ),
+        end_date=reference_date,
+    )
+    spread_rate_historical_yield = market_overview_services.get_historical_prices(
+        front_data["spread_rate"],
+        start_date=_get_start_date(
+            reference_date, front_data["spread_rates_chart_duration"]
+        ),
+        end_date=reference_date,
+    )
+    spread_rate_df = pd.merge(
+        pd.DataFrame(main_rate_historical_yield),
+        pd.DataFrame(spread_rate_historical_yield),
+        on="price_date",
+        how="left",
+        suffixes=("_current", "_prev"),
+    )
+    spread_rate_df["price"] = (
+        spread_rate_df["price_current"] - spread_rate_df["price_prev"]
+    )
+    return {
+        "stock_prices": market_overview_services.get_historical_prices(
+            front_data["stock_name"],
+            start_date=_get_start_date(
+                reference_date, front_data["stock_chart_duration"]
+            ),
+            end_date=reference_date,
+        ),
+        "stock_prices_compare": (
+            market_overview_services.get_historical_prices(
+                front_data["stock_name_compare"],
+                start_date=_get_start_date(
+                    reference_date, front_data["stock_chart_duration"]
+                ),
+                end_date=reference_date,
+            )
+            if front_data["stock_name_compare"]
+            else None
+        ),
+        "fx_prices": market_overview_services.get_historical_prices(
+            front_data["fx_name"],
+            start_date=_get_start_date(reference_date, front_data["fx_chart_duration"]),
+            end_date=reference_date,
+        ),
+        "fx_prices_compare": (
+            market_overview_services.get_historical_prices(
+                front_data["fx_name_compare"],
+                start_date=_get_start_date(
+                    reference_date, front_data["fx_chart_duration"]
+                ),
+                end_date=reference_date,
+            )
+            if front_data["fx_name_compare"]
+            else None
+        ),
+        "crypto_prices": market_overview_services.get_historical_prices(
+            front_data["crypto_name"],
+            start_date=_get_start_date(
+                reference_date, front_data["crypto_chart_duration"]
+            ),
+            end_date=reference_date,
+        ),
+        "crypto_prices_compare": (
+            market_overview_services.get_historical_prices(
+                front_data["crypto_name_compare"],
+                start_date=_get_start_date(
+                    reference_date, front_data["crypto_chart_duration"]
+                ),
+                end_date=reference_date,
+            )
+            if front_data["crypto_name_compare"]
+            else None
+        ),
+        "commodity_prices": market_overview_services.get_historical_prices(
+            front_data["commodity_name"],
+            start_date=_get_start_date(
+                reference_date, front_data["commodity_chart_duration"]
+            ),
+            end_date=reference_date,
+        ),
+        "commodity_prices_compare": (
+            market_overview_services.get_historical_prices(
+                front_data["commodity_name_compare"],
+                start_date=_get_start_date(
+                    reference_date, front_data["commodity_chart_duration"]
+                ),
+                end_date=reference_date,
+            )
+            if front_data["commodity_name_compare"]
+            else None
+        ),
+        "reference_yield_curve": market_overview_services.get_yield_curve(
+            reference_date,
+            LocationChoices.from_label(front_data["yield_curve_location"]),
+        ),
+        "previous_yield_curve": market_overview_services.get_yield_curve(
+            previous_curve_date,
+            LocationChoices.from_label(front_data["yield_curve_location"]),
+        ),
+        "spread_rates": spread_rate_df[["price_date", "price"]].to_dict(
+            orient="records"
+        ),
+    }
--- data_visualization/urls.py
+++ data_visualization/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from data_visualization.views import (
-    data_visualization_view,
     economic_recap_view,
+    market_charts_view,
     market_recap_view,
 )
 
 urlpatterns = [
     path("market-recap/", market_recap_view, name="market-recap"),
-    path("market-charts/", data_visualization_view, name="data-visualization"),
+    path("market-charts/", market_charts_view, name="market-charts"),
     path("economic-recap/", economic_recap_view, name="economic-recap-view"),
 ]
